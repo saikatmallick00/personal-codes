bd5ff2ca4c582f19c054bbbfb990d2e617304aef
Saikat Mallick <saikatmallick145@gmail.com>
2025-05-18 10:19:20 +0530

    Add files via upload

---
+++ CHECK_THE_ARRAY_SORTED_OR_NOT.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int a[20],n,i;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("\nEnter %d element of an array: ",i);
+        scanf("%d",&a[i]);
+    }
+    a[1]=max
+    a[1]=min
+    if (a[0]>a[1])
+    {
+        for ( i = 1; i < n; i++)
+        {
+            if (a[i]>a[i+1])
+            {
+                /* code */
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ SORTING_ARRAY_IN_DESCENDING_ORDER.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a[100],n,i;
+    printf("enter the size of array: ");
+    scanf("%d",&n);
+    for ( i = 0; i < n; i++)
+    {
+        printf("\nenter %d element: ",i);
+        scanf("%d",&a[i]);
+    }
+    for ( i = 0; i < n-1; i++)
+    {
+        if (a[i])
+        {
+            /* code */
+        }
+        
+    }
+    
+}
